cf9773b01d601ea29eff80b69ccabf29f511b26e
Ivan Miric <imiric@gmail.com>
2013-08-16 22:56:37 +0200

    Added .on(), .between() and .starting() features

--- schedule/__init__.py
+++ schedule/__init__.py
@@ -37,6 +37,7 @@ Usage:
 import datetime
 import functools
 import logging
+import random
 import time
 
 logger = logging.getLogger('schedule')
@@ -81,6 +82,15 @@ class Scheduler(object):
         self.jobs.append(job)
         return job
 
+    def on(self, *days):
+        """Schedule a new job to run on specific weekdays.
+
+        See the docstring for `Job.on()`.
+        """
+        job = self.every()
+        job.unit = 'days'
+        return job.on(*days)
+
     @property
     def next_run(self):
         """Datetime when the next job should run."""
@@ -96,11 +106,17 @@ class Scheduler(object):
 
 class Job(object):
     """A periodic job as used by `Scheduler`."""
+    WEEKDAYS = {'sunday': 0, 'monday': 1, 'tuesday': 2, 'wednesday': 3,
+                'thursday': 4, 'friday': 5, 'saturday': 6}
+
     def __init__(self, interval):
         self.interval = interval  # pause interval * unit between runs
         self.job_func = None  # the job job_func to run
         self.unit = None  # time units, e.g. 'minutes', 'hours', ...
         self.at_time = None  # optional time at which this job runs
+        self.between_times = ()
+        self.run_days = []
+        self.start_run = None  # datetime after which this job will start
         self.last_run = None  # datetime of the last run
         self.next_run = None  # datetime of the next run
         self.period = None  # timedelta between runs, only valid for
@@ -184,6 +200,33 @@ class Job(object):
         self.unit = 'weeks'
         return self
 
+    def on(self, *days):
+        """Schedule the job to run on specific weekdays.
+
+        `days` can be a string (or sequence of strings) with the name of the
+        weekday (case insensitive), e.g. 'Monday', 'sunday', etc, or a starting
+        substring of the name of the weekday, e.g.  'tue', 'Sat', etc.
+
+        If you specify multiple days, e.g. ('mon', 'wed'), the job will run
+        every Monday and Wednesday.
+
+        You can also specify OR conditions by separating the day names with a
+        pipe, e.g. ('sun|mon', 'wed|thu'). In this case the job will run
+        every Sunday *or* Monday, and every Wednesday *or* Thursday.
+        """
+        weeknums = []
+        for day in days:
+            day_or = set()
+            for d in day.split('|'):
+                for n, i in Job.WEEKDAYS.items():
+                    if n.startswith(d.lower()):
+                        day_or.add(i)
+            if day_or:
+                weeknums.append(day_or)
+
+        self.run_days = weeknums
+        return self
+
     def at(self, time_str):
         """Schedule the job every day at a specific time.
 
@@ -195,6 +238,17 @@ class Job(object):
         assert 0 <= hour <= 23
         assert 0 <= minute <= 59
         self.at_time = datetime.time(hour, minute)
+        return self
+
+    def between(self, time_str):
+        """Schedule the job at a random time between two timestamps."""
+        start, end = [datetime.datetime.strptime(t, '%H:%M')
+                      for t in time_str.split('-')]
+        self.between_times = (start, end)
+        return self
+
+    def starting(self, date_str):
+        self.start_run = datetime.datetime.strptime(date_str, '%Y-%m-%d')
         return self
 
     def do(self, job_func, *args, **kwargs):
@@ -226,17 +280,55 @@ class Job(object):
         # Allow *, ** magic temporarily:
         # pylint: disable=W0142
         assert self.unit in ('seconds', 'minutes', 'hours', 'days', 'weeks')
+        starting = self.start_run or datetime.datetime.now()
+
         self.period = datetime.timedelta(**{self.unit: self.interval})
-        self.next_run = datetime.datetime.now() + self.period
+        self.next_run = starting + self.period
+
+        if self.run_days:
+            run_days = self.run_days[:]
+            if self.last_run:
+                starting = self.last_run
+                # Don't consider this day group if it has been run already
+                for day in self.run_days:
+                    if self.last_run.isoweekday() in day:
+                        run_days.remove(day)
+
+            days = set()
+            for day in run_days:
+                days.add(random.sample(day, 1)[0])
+
+            if not days:
+                days_delta = 0
+            else:
+                # Calculate the closest day from the starting date
+                delta_all = sorted([(i - starting.isoweekday()) % 7
+                                    for i in days])
+                days_delta = delta_all[0]
+
+            if (days_delta == 0 and self.last_run and
+                    self.last_run.date() == starting.date()):
+                # Make sure the job doesn't run today twice
+                if self.unit == 'days':
+                    days_delta = 7
+                elif self.unit == 'weeks':
+                    days_delta = self.interval * 7
+            self.next_run = starting + datetime.timedelta(days=days_delta)
+
+        if self.between_times:
+            start, end = self.between_times
+            # Choose a random time between both timestamps
+            self.at_time = (start + datetime.timedelta(
+                seconds=random.randint(0, int(
+                    (end - start).total_seconds())))).time()
         if self.at_time:
-            assert self.unit == 'days'
             self.next_run = self.next_run.replace(hour=self.at_time.hour,
                                                   minute=self.at_time.minute,
                                                   second=self.at_time.second,
                                                   microsecond=0)
             # If we are running for the first time, make sure we run
             # at the specified time *today* as well
-            if (not self.last_run and
+            if (not self.last_run and not self.run_days and
                     self.at_time > datetime.datetime.now().time()):
                 self.next_run = self.next_run - datetime.timedelta(days=1)
 
@@ -251,6 +343,14 @@ jobs = default_scheduler.jobs  # todo: should this be a copy, e.g. jobs()?
 def every(interval=1):
     """Schedule a new periodic job."""
     return default_scheduler.every(interval)
+
+
+def on(*days):
+    """Schedule a new job to run on specific weekdays.
+
+    See the docstring for `Job.on()`.
+    """
+    return default_scheduler.on(*days)
 
 
 def run_pending():
